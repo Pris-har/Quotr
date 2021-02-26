e59f8e2fc695d9a09610a4ad588d6a5840970bd2
Pris-har <74337506+Pris-har@users.noreply.github.com>
2021-02-25 22:53:19 -0600

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 //conexion remota
 define("DB_HOST", "remotemysql.com");
 define("DB_NAME", "Pua9qGgCRT");
-define("DB_USER", "remotemysql.com");
+define("DB_USER", "Pua9qGgCRT");
 define("DB_PASS", "WxdZ2hLjfr");
  
 ?>
