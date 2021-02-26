1e637b7ab34f49dcd431614b4901e6372f0a8aac
Pris-har <74337506+Pris-har@users.noreply.github.com>
2021-02-25 22:36:29 -0600

    Update conexion.php

--- config/conexion.php
+++ config/conexion.php
@@ -1,8 +1,8 @@
-﻿<?php
-	# conectare la base de datos
+<?php
+	
     $con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     if(!$con){
-        die("imposible conectarse: ".mysqli_error($con));
+        die("Imposible conectarse: ".mysqli_error($con));
     }
     if (@mysqli_connect_errno()) {
         die("Conexión falló: ".mysqli_connect_errno()." : ". mysqli_connect_error());
