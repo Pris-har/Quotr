def232ccd24872bd4e45d26b5749a85973b4b637
Pris-har <74337506+Pris-har@users.noreply.github.com>
2021-03-06 01:52:46 -0600

    Add files via upload

---
+++ config/registro_usuario.php
@@ -0,0 +1,50 @@
+<?php
+	
+	include 'config/conexion.php';
+	
+	$nombre = $_POST['nombres'];
+	$apellido = $_POST['apellidos'];
+	$empresa = $_POST['empresa'];
+	$correo = $_POST['correo'];
+	$contrasena = $_POST['contrasena'];
+	
+	//CIFRAR CONTRASEÑA
+	$contrasena = hash('sha512', $contrasena);
+	
+	$query = "INSERT INTO usuario(nombre, apellido, empresa, correo, contrasena) 
+			  VALUES('$nombre', '$apellido', '$empresa', '$correo', '$contrasena')";
+	
+	//VERIFICAR QUE EL CORREO NO SE REPITA EN LA BASE DE DATOS
+	
+	$verificar_correo = mysqli_query($conexion, "SELECT * FROM usuario WHERE correo='$correo' ");
+	
+	if(mysqli_num_rows($verificar_correo) > 0){
+		echo '
+			<script>
+				alert("Este correo ya está registrado, intenta con otro diferente");
+				window.location = "index.php";
+			</script>
+		';
+		exit();
+	}
+	
+	$ejecutar = mysqli_query($conexion, $query);
+	
+	if($ejecutar){
+		echo '
+			<script>
+				alert("Usuario almacenado exitosamente");
+				window.location = "index.php";
+			</script>
+		';	
+	}else{
+		echo '
+			<script>
+				alert("Inténtalo de nuevo, usuario no almacenado");
+				window.location = "index.php";
+			</script>
+		';
+	}
+	
+	mysqli_close($conexion);
+?>
