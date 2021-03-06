1f43bb8eba82781dcd2a6ea3e6d790aeb0163c83
Pris-har <74337506+Pris-har@users.noreply.github.com>
2021-03-06 01:57:51 -0600

    Update registro_usuario.php

--- config/registro_usuario.php
+++ config/registro_usuario.php
@@ -1,6 +1,6 @@
 <?php
 	
-	include 'config/conexion.php';
+	include 'conexion.php';
 	
 	$nombre = $_POST['nombres'];
 	$apellido = $_POST['apellidos'];
@@ -16,35 +16,35 @@
 	
 	//VERIFICAR QUE EL CORREO NO SE REPITA EN LA BASE DE DATOS
 	
-	$verificar_correo = mysqli_query($conexion, "SELECT * FROM usuario WHERE correo='$correo' ");
+	$verificar_correo = mysqli_query($con, "SELECT * FROM usuario WHERE correo='$correo' ");
 	
 	if(mysqli_num_rows($verificar_correo) > 0){
 		echo '
 			<script>
 				alert("Este correo ya está registrado, intenta con otro diferente");
-				window.location = "index.php";
+				window.location = "../index.php";
 			</script>
 		';
 		exit();
 	}
 	
-	$ejecutar = mysqli_query($conexion, $query);
+	$ejecutar = mysqli_query($con, $query);
 	
 	if($ejecutar){
 		echo '
 			<script>
 				alert("Usuario almacenado exitosamente");
-				window.location = "index.php";
+				window.location = "../index.php";
 			</script>
 		';	
 	}else{
 		echo '
 			<script>
 				alert("Inténtalo de nuevo, usuario no almacenado");
-				window.location = "index.php";
+				window.location = "../index.php";
 			</script>
 		';
 	}
 	
-	mysqli_close($conexion);
+	mysqli_close($con);
 ?>
