ecafb4b09d0cf1928b1fd23abb8abe5572aee94e
Pris-har <74337506+Pris-har@users.noreply.github.com>
2021-03-06 03:05:42 -0600

    Update registro_usuario.php

--- config/registro_usuario.php
+++ config/registro_usuario.php
@@ -1,6 +1,6 @@
 <?php
-	require_once ("db.php");//Contiene las variables de configuracion para conectar a la base de datos
-	require_once ("conexion.php");//Contiene funcion que conecta a la base de datos
+	include 'db.php';//Contiene las variables de configuracion para conectar a la base de datos
+	include 'conexion.php';//Contiene funcion que conecta a la base de datos
 	//include 'conexion.php';
 	
 	$nombre = $_POST['nombres'];
