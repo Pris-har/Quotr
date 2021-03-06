4d352464207707e9fb5efbe995a2fc3647825a3d
Pris-har <74337506+Pris-har@users.noreply.github.com>
2021-03-06 03:19:28 -0600

    Update login_usuario.php

--- config/login_usuario.php
+++ config/login_usuario.php
@@ -3,8 +3,10 @@
 	session_start();
 	//include 'conexion.php';
 	
-        require_once ("db.php");//Contiene las variables de configuracion para conectar a la base de datos
-	require_once ("conexion.php");//Contiene funcion que conecta a la base de datos
+        //require_once ("db.php");//Contiene las variables de configuracion para conectar a la base de datos
+	//require_once ("conexion.php");//Contiene funcion que conecta a la base de datos
+	include 'db.php';//Contiene las variables de configuracion para conectar a la base de datos
+	include 'conexion.php';//Contiene funcion que conecta a la base de datos
 
 	$correo = $_POST['correo'];
 	$contrasena = $_POST['contrasena'];
@@ -24,7 +26,7 @@
 			</script>
 		
 		';
-		exit();
+		
 		
 	}
 	
