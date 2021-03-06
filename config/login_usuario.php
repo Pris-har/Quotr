a28c6ff7c20b35d326acdce9057268dcc6f60e03
Pris-har <74337506+Pris-har@users.noreply.github.com>
2021-03-06 01:55:53 -0600

    Update login_usuario.php

--- config/login_usuario.php
+++ config/login_usuario.php
@@ -1,23 +1,23 @@
 <?php
 
 	session_start();
-	include 'config/conexion.php';
+	include 'conexion.php';
 	
 	$correo = $_POST['correo'];
 	$contrasena = $_POST['contrasena'];
 	$contrasena = hash('sha12', $contrasena);
 	
-	$validar_login = mysqli_query($conexion, "SELECT * FROM usuario WHERE correo='$correo' and contrasena='$contrasena'");
+	$validar_login = mysqli_query($con, "SELECT * FROM usuario WHERE correo='$correo' and contrasena='$contrasena'");
 	
 	if(mysqli_num_rows($validar_login) > 0){
 		$_SESSION['usuario'] = $correo;
-		header("location: cotizador.php");
+		header("location: ../cotizador.php");
 		exit;
 	}else{
 		echo '
 			<script>
 				alert("Usuario no existe, por favor verifique los datos introducidos");
-				windows.location = "index.php";
+				windows.location = "../index.php";
 			</script>
 		
 		';
