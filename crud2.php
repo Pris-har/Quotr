0a94b49b55ce51f2c5c098495f16375b6d932b5f
Prissila Pantoja <art.foundr.self@gmail.com>
2021-03-22 21:47:43 -0600

    cambios

--- crud2.php
+++ crud2.php
@@ -89,7 +89,7 @@
     ?>
     <div class="row justify-content-center">
     <form action="crud_process.php" method="POST">
-        <input type="hidden" name="id" value="<?php echo $id_producto; ?>">
+        <input type="hidden" name="id_producto" value="<?php echo $id_producto; ?>">
         <div class="form-group">
         <label>Código:</label>
         <input type="text" name="codigo" class="form-control" 
